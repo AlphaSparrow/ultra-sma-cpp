e9e92d2d0acaeaa2b94097df5c1e74ba4b25913d
Sparrow <theaayushsakariya@gmail.com>
2026-05-15 11:56:04 +0530

    Time tracking cool nanonseconds and microseconds using chrono

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <chrono>
 #include "parser.hpp"
 #include "calculator.hpp"
 
@@ -16,11 +17,22 @@ int main(){
         return 1;
     }
 
+    // Start Clock just before the W16 Quad Turbo Engine
+    auto start = std::chrono::high_resolution_clock::now();
+
     auto fastSma = Calculator::calculateSMA(prices, 3);
     auto slowSma = Calculator::calculateSMA(prices, 5);
     auto ema     = Calculator::calculateEMA(prices, 5);
 
     auto signals = Calculator::generateSignals(fastSma, slowSma);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    if(!signals.empty()){
+        std::cout << "(Engine internal check: " << (int)signals[0] << ")" << '\n';
+    }
+
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 
     std::cout << std::fixed << std::setprecision(2);
 
@@ -34,21 +46,26 @@ int main(){
               << "SIGNALSS" << '\n';
 
     std::cout << std::string(78, '=') << '\n';
-
+    
     for(size_t i = 0; i < prices.size(); ++i){
         std::string sigText = "HOLD";
         if(signals[i] == Signal::BUY) sigText = "BUY";
         if(signals[i] == Signal::SELL) sigText = "SELL";
-
+        
         std::cout << std::left  << std::setw(12) << prices[i].date  << " | "
-                  << std::right << std::setw(10) << prices[i].close << " | "
-                  << std::right << std::setw(10) << fastSma[i]      << " | "
-                  << std::right << std::setw(10) << slowSma[i]      << " | "
-                  << std::right << std::setw(10) << ema[i]          << " | "
-                  << sigText << '\n'; 
+        << std::right << std::setw(10) << prices[i].close << " | "
+        << std::right << std::setw(10) << fastSma[i]      << " | "
+        << std::right << std::setw(10) << slowSma[i]      << " | "
+        << std::right << std::setw(10) << ema[i]          << " | "
+        << sigText << '\n'; 
     }
-
+    
+    std::cout << std::string(78, '-') << '\n';
     std::cout << "Completed itna: " << prices.size() << "rowss" << '\n';
+
+    std::cout << "Latency Time baby" << '\n';
+    std::cout << "Time taken: " << duration.count() << "nanoseconds" << '\n';
+    std::cout << std::string(78, '=') << '\n';
 
     return 0;
 }
